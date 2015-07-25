9a62e39d96984a52f560b1b83a28ebc0b2d4481b
jmsiqueiros <jmsiqueiros.bioetica@gmail.com>
2015-07-24 21:45:33 -0500

    Code is working for two column data, separated by a pipe |.

--- text2json.py
+++ text2json.py
@@ -3,12 +3,13 @@
 import csv
 
 # save the json output as emp.json 
-jsfile = file('emp.json', 'w')
+jsfile = file('salud_ti.json', 'w')
 jsfile.write('[\r\n')
 
-with open('employee.txt','r') as f:
+with open('salud_MX_ti.csv','r') as f:
     next(f) # skip headings
-    reader=csv.reader(f,delimiter='\t')
+    #reader=csv.reader(f,delimiter='\t')
+    reader=csv.reader(f,delimiter='|')
 
     # get the total number of rows excluded the heading
     row_count = len(list(reader))
@@ -18,19 +19,19 @@ with open('employee.txt','r') as f:
     f.seek(0)
     next(f) # skip headings
     
-    for name,empid,dept in reader:
+    for UT,TI in reader:
 
         ite+= 1
         
         jsfile.write('\t{\r\n')
         
-        n = '\t\t\"name\": \"' + name + '\",\r\n'
-        i = '\t\t\"empid\": \"' + empid + '\",\r\n'
-        d = '\t\t\"department\": \"' + dept + '\"\r\n'
+        u = '\t\t\"UT\": \"' + UT + '\",\r\n'
+        t = '\t\t\"TI\": \"' + TI + '\",\r\n'
+        #d = '\t\t\"department\": \"' + dept + '\"\r\n'
        
-        jsfile.write(n)
-        jsfile.write(i)
-        jsfile.write(d)
+        jsfile.write(u)
+        jsfile.write(t)
+        #jsfile.write(d)
 
         jsfile.write('\t}')
 
